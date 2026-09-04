55040985332affa2e94c97c38b41613756e2cf2d
yuji9993 <yuji9993@gmail.com>
2026-09-04 17:00:58 +0900

    히어로 영역 스와이퍼 라이브러리 작업중

--- js/script.js
+++
@@ -1,3 +0,0 @@
-document.querySelector("h1").addEventListener("click", function () {
-  alert("안녕하세요!");
-});
---
+++ js/swHero.js
@@ -0,0 +1,10 @@
+const swHero = new Swiper(".sw-hero", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".hero-next-btn",
+    prevEl: ".hero-prev-btn",
+  },
+});
